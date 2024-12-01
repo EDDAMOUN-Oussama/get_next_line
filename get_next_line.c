c47857565ebe0c82c8d75b356bb31ba81027398a
Oussama Eddamoun <oeddamou@c2r7p1.1337.ma>
2024-12-01 21:47:55 +0100

    get

--- get_next_line.c
+++ get_next_line.c
@@ -6,75 +6,146 @@
 /*   By: oeddamou <oeddamou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 12:28:46 by oeddamou          #+#    #+#             */
-/*   Updated: 2024/11/26 17:46:24 by oeddamou         ###   ########.fr       */
+/*   Updated: 2024/12/01 21:39:21 by oeddamou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
-int ft_substr(char *buf, int s, char **p)
+int	ft_strcpy(char *d, char *s)
 {
-    int i;
-    int c;
+	int	i;
 
-    i = 0;
-    c = 0;
-    while (i < s)
-    {
-        if (buf[i] == '\n')
-            {
-                c = 1;
-                break;
-            }
-        i++;
-    }
-    (*p) = malloc (i + 1);
-    if (!(*p))
-        return (-1);
-    (*p)[i + 1] = '\0';
-    while (i >= 0)
-    {
-        (*p)[i] = buf[i];
-        i--;
-    }
-    return (c);
+	i = 0;
+	if (s)
+	{
+		i = 0;
+		while (s[i])
+		{
+			d[i] = s[i];
+			if (s[i] == '\n')
+			{
+				i++;
+				break ;
+			}
+			i++;
+		}
+	}
+	d[i] = '\0';
+	return (i);
 }
-int ft_strjoin(char **r, char *buf, int s)
-{
-    char    *p;
-    int j;
-    int k;
 
-    j = 0;
-    while ((*r)[j++]);
-    if (!p)
-    return ((free(*r), -1));
-    while (k < j)
-        {
-            p[k] = (*r)[k];
-            k++;
-        }
-        k = 0;
-    p[k] = '\0';
-    free (*r);
-    *r = p;
-    if (p[k - 1] = '\n')
-        return (1);
-    return (0);
+int	ft_substr(char **r)
+{
+	char	*p;
+	int		i;
+	int 	s;
+
+	i = ((s = 0), 0);
+	while ((*r)[i])
+	{
+		if ((*r)[i] == '\n')
+		{
+			i++;
+			break ;
+		}
+		i++;
+	}
+	while ((*r)[i + s])
+		s++;
+		if (s == 0)
+			return (-1);
+	p = malloc(s + 1);
+	if (!p)
+		return (-1);
+	s = i;
+	ft_strcpy(p, (*r) + i);
+	(*r) = ((free((*r))), p);
+	return (s);
 }
-char    *get_next_line(int fd)
-{
-    char buf[BUFFER_SIZE];
-    char    *r;
-    int i;
 
-    r = NULL;
-    i = read(fd, buf, BUFFER_SIZE);
-    if (i <= 0)
-        return (NULL);
-    while (i > 0)
-    {
-        ft_substr(buf, i, &r);
-    }
-    
+int	ft_strjoin(char **buf, char *r)
+{
+	char	*p;
+	int		i;
+	int		n;
+
+	i = ((n = 0), 0);
+	while (r[i])
+		if (r[i] != '\n')
+			i++;
+	else
+	{
+		i++;
+		break ;
+	}
+	if ((*buf))
+		while ((*buf)[n] != '\0')
+			n++;
+	p = malloc(i + n + 1);
+	if (!p)
+		return (-1);
+	ft_strcpy(p, *buf);
+	ft_strcpy(p + n, r);
+	*buf = ((free(*buf)), p);
+	if (p[n + i - 1] == '\n')
+		return (1);
+	return (0);
+}
+char	*ft_read_line(int fd, char **buf)
+{
+	char	*r;
+	int		s;
+
+	r = malloc(BUFFER_SIZE + 1);
+	if (!r)
+		return (NULL);
+	r[BUFFER_SIZE] = '\0';
+	s = 0;
+	while (!s)
+	{
+		s = read(fd, r, BUFFER_SIZE);
+		if (s == -1 || s == 0)
+			break ;
+		r[s] = '\0';
+		s = ft_strjoin(buf, r);
+	}
+	if (s == -1)
+	{
+		free(*buf);
+		free(r);
+		*buf = NULL;
+		r = NULL;
+	}
+	if (s == 0)
+	{
+		free(r);
+		r = NULL;
+		// ft_strjoin(&r, *buf);
+		 
+	}
+	return (r);
+}
+char	*get_next_line(int fd)
+{
+	char	*buf;
+	static char		*r;
+	int			c;
+
+	if (r)
+	{
+		c = ft_substr(&r);
+		if (c == -1)
+		{
+			free(r);
+			r = NULL;
+			return (NULL);
+		}
+
+	}
+	buf = r;
+	r = ft_read_line(fd, &buf);
+	
+	return (buf);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,10 +1,11 @@
 #ifndef _GET_NEXT_LINE_H_
 # define _GET_NEXT_LINE_H_
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
+# define BUFFER_SIZE 11
 #endif
 # include <unistd.h>
 # include <stdlib.h>
 
 char    *get_next_line(int fd);
+
 #endif
